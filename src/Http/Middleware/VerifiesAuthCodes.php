1e4b2d150088514fdbc20c7579d9ab71cb95476c
D. Nagy Gergő <hello@iamgergo.com>
2025-08-10 18:46:59 +0200

    cleanup

--- src/Http/Middleware/VerifiesAuthCodes.php
+++ src/Http/Middleware/VerifiesAuthCodes.php
@@ -3,10 +3,9 @@
 namespace Cone\Laravel\Auth\Http\Middleware;
 
 use Closure;
-use Cone\Laravel\Auth\Interfaces\Responses\AuthCodeVerifyResponse;
 use Cone\Laravel\Auth\Interfaces\VerifiesAuthCodes as Contract;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Redirect;
 use Symfony\Component\HttpFoundation\Response;
 
 class VerifiesAuthCodes
@@ -18,10 +17,14 @@ class VerifiesAuthCodes
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if (! $request->user() instanceof Contract || ! $request->user()->verifiesAuthCodes()) {
-            return App::make(AuthCodeVerifyResponse::class)->toResponse($request);
+        if (! $request->user() instanceof Contract
+            || ! $request->user()->verifiesAuthCodes()
+            || $request->cookie('device_token') === sha1(sprintf('%s:%s', $request->user()->getKey(), $request->user()->email))
+            || $request->session()->has('auth.code-verified')
+        ) {
+            return $next($request);
         }
 
-        return $next($request);
+        return Redirect::route('auth.auth-code.show');
     }
 }
